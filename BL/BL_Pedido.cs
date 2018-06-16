5276a60701342579048a05c32a9fd90027bd59f6
Fabián Jiménez <fabian.jimenez@ucrso.info>
2018-06-15 23:03:24 -0600

    Creacion de objetos

---
+++ BL/BL_Pedido.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BL
+{
+    public class BL_Pedido
+    {
+        public string cliente { get; set; }
+        public string plato { get; set; }
+        public DateTime fecha { get; set; }
+        public string estado { get; set; }
+
+        public BL_Pedido()
+        {
+
+        }
+
+        public BL_Pedido(string cliente, string plato, DateTime fecha, string estado)
+        {
+            this.cliente = cliente;
+            this.plato = plato;
+            this.fecha = fecha;
+            this.estado = estado;
+        }
+    }
+}
---
+++ TO/TO_Plato.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TO
+{
+    public class TO_Plato
+    {
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public double precio { get; set; }
+        public string foto { get; set; }
+        public string estado { get; set; }
+
+        public TO_Plato()
+        {
+
+        }
+
+        public TO_Plato(string nombre, string descripcion, double precio, string foto, string estado)
+        {
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+            this.foto = foto;
+            this.estado = estado;
+        }
+    }
+}
