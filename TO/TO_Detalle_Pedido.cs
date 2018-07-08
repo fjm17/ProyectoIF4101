e4aec93d0f698366c802f5e57b358bb6e37e0055
jrvz9714 <jrvz9714@hotmail.com>
2018-07-08 00:14:32 -0600

    Prueba 2

---
+++ TO/TO_Detalle_Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TO
+{
+    public class TO_Detalle_Pedido
+    {
+        public int NumeroPedido { get; set; }
+        public string NombrePlato { get; set; }
+
+        public TO_Detalle_Pedido(int numeroPedido, string nombrePlato)
+        {
+            this.NumeroPedido = numeroPedido;
+            this.NombrePlato = nombrePlato;
+        }
+
+        public TO_Detalle_Pedido()
+        {
+
+        }
+    }
+}
---
+++ TO/TO_Manejador_Lista_Platos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TO
+{
+    public class TO_Manejador_Lista_Platos
+    {
+        public List<TO_Plato> Platos { get; set; }
+
+        public TO_Manejador_Lista_Platos()
+        {
+            Platos = new List<TO_Plato>();
+        }
+
+        public void AgregarPlato(TO_Plato entrada)
+        {
+            Platos.Add(entrada);
+        }
+    }
+}
