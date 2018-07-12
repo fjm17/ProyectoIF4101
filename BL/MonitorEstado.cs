e076355b92b527d556f524a7d7d4a7450cce6996
richardbomo26 <richardbomo26@gmail.com>
2018-07-11 22:19:29 -0500

    11JulioNoche2

--- BL/MonitorEstado.cs
+++ BL/MonitorEstado.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using DAO;
 
 namespace BL
 {
@@ -11,26 +12,48 @@ namespace BL
         private Timer tiempo;
         private int maximo;
         private int contador;
+        private int numeroPedido;
+        private string temporal;
 
-        public MonitorEstado(int max)
+        public MonitorEstado(int numero)
         {
-            tiempo = new Timer(controlar, null, 0, 1000);
-            maximo = max;
+            tiempo = new Timer(controlar, null, 1000, 1000);
+            maximo = obtenerTiempo("A Tiempo");
+            temporal = "A Tiempo";
             contador = 0;
+            numeroPedido = numero;
         }
 
         private void controlar(object args)
         {
             if (contador == maximo)
             {
-                tiempo.Dispose();
-                Console.WriteLine("Aqui termina");
+                DAOPedido dao = new DAOPedido();
+
+                if (temporal.Equals("A Tiempo"))
+                {
+                    maximo = obtenerTiempo("Sobre Tiempo");
+                    temporal = "Sobre Tiempo";
+                    dao.CambiarEstado(numeroPedido, 4);
+                }
+
+                if (temporal.Equals("Sobre Tiempo"))
+                {
+                    maximo = obtenerTiempo("Demorado");
+                    temporal = "Demorado";
+                    dao.CambiarEstado(numeroPedido, 5);
+                    tiempo.Dispose();
+                }
             }
-            else
-            {
                 contador++;
-                Console.WriteLine("Va por el: " + contador);
-            }
         }
+
+        private int obtenerTiempo(string estado)
+        {
+            DAOPedido daoPedido = new DAOPedido();
+            return daoPedido.ObtenerTiempo(estado);
+        }
+
+
     }
 }
